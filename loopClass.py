38aa955263b0779ec5b749311ee67ad958792727
satyam <39213202+krsatyam20@users.noreply.github.com>
2021-01-27 21:18:37 +0530

    Add files via upload

---
+++ loopClass.py
@@ -0,0 +1,34 @@
+''' loop
+loop:Repeating : Some specific code/syntax run severalnumber of time
+    for
+    while
+    do while : which is not suported by python '''
+
+
+print("Abhishek")
+
+for i in range(1,11):
+    print("Abhishek",i)
+
+
+''' 2* 1= 2
+    2* 2= 4
+    2* 3= 6
+    '''
+
+for i in range(1,11):
+    print("2 *", i,"=", i*2)
+
+
+for i in range(1,20):
+    if i%2!=0 :
+        print(i ,"Even Number")
+
+
+
+
+
+
+
+
+    
